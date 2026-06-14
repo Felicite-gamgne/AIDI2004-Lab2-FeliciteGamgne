41663b29e8bf7c16229a09e18a30614f48cdb50e
Felicite Gamgne <felice.gamgne@gmail.com>
2026-06-13 22:07:29 -0400

    Initial commit - Felicite Gamgne

---
+++ README.md
@@ -0,0 +1,2 @@
+# AIDI2004 Lab 2 - Felicite Gamgne
+GitHub Actions CI lab for AIDI 2004.
---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py - Created by Firstname Lastname
+def greet(name):
+    return f"Hello, {name}! Welcome to AIDI 2004."
+
+if __name__ == "__main__":
+    print(greet("Felicite"))
