8007bc479a39fa1bfb02c7dcf6f5dab07192afe2
Felicite Gamgne <felice.gamgne@gmail.com>
2026-06-13 22:31:11 -0400

    Update greeting message - Felicite Gamgne

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py - Created by Firstname Lastname
 def greet(name):
-    return f"Hello, {name}! Welcome to AIDI 2004."
+    return f"Hello my dear {name}! Welcome to AIDI 2004. I wish you a great semester ahead! "
 
 if __name__ == "__main__":
     print(greet("Felicite"))
